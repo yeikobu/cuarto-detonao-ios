c451942902ea71358014671b8e2cb2c5026c00f1
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-10-10 00:36:39 -0300

    Agrega sección para agregar las rosas

--- cuarto-detonao/View/NewReserveView.swift
+++ cuarto-detonao/View/NewReserveView.swift
@@ -20,6 +20,13 @@ struct NewReserveView: View {
     @State private var receiverNickName = ""
     @State private var receiverCourse = ""
     
+    @State private var redRosesQuantity = 0
+    @State private var orangeRosesQuantity = 0
+    @State private var blueRosesQuantity = 0
+    @State private var purpleRosesQuantity = 0
+    @State private var yellowRosesQuantity = 0
+    @State private var whiteRosesQuantity = 0
+    
     let courses = [ "Séptimo A", "Séptimo B", "Octavo A", "Octavo B", "Primero Medio A", "Primero Medio B", "Primero Medio C", "Primero Medio D", "Segundo Medio A", "Segundo Medio B", "Segundo Medio C", "Segundo Medio D", "Tercero Medio A", "Tercero Medio B", "Tercero Medio C", "Tercero Medio D", "Cuarto Medio A", "Cuarto Medio B", "Cuarto Medio C", "Cuarto Medio D", "Profesor", "Personal del liceo"]
     
     var body: some View {
@@ -73,6 +80,76 @@ struct NewReserveView: View {
                     
                 } header: {
                     Text("Datos del receptor")
+                }
+                
+                Section {
+                    Stepper(value: $redRosesQuantity, in: 0...20) {
+                        HStack {
+                            Text("Rojas")
+                            
+                            Spacer()
+                            
+                            Text("\(redRosesQuantity)")
+                                .padding(.trailing, 4)
+                        }
+                    }
+                    
+                    Stepper(value: $orangeRosesQuantity, in: 0...20) {
+                        HStack {
+                            Text("Naranja puntas rojas")
+                            
+                            Spacer()
+                            
+                            Text("\(orangeRosesQuantity)")
+                                .padding(.trailing, 4)
+                        }
+                    }
+                    
+                    Stepper(value: $blueRosesQuantity, in: 0...20) {
+                        HStack {
+                            Text("Azules")
+                            
+                            Spacer()
+                            
+                            Text("\(blueRosesQuantity)")
+                                .padding(.trailing, 4)
+                        }
+                    }
+                    
+                    Stepper(value: $purpleRosesQuantity, in: 0...20) {
+                        HStack {
+                            Text("Moradas")
+                            
+                            Spacer()
+                            
+                            Text("\(purpleRosesQuantity)")
+                                .padding(.trailing, 4)
+                        }
+                    }
+                    
+                    Stepper(value: $yellowRosesQuantity, in: 0...20) {
+                        HStack {
+                            Text("Amarillas")
+                            
+                            Spacer()
+                            
+                            Text("\(yellowRosesQuantity)")
+                                .padding(.trailing, 4)
+                        }
+                    }
+                    
+                    Stepper(value: $whiteRosesQuantity, in: 0...20) {
+                        HStack {
+                            Text("Blancas")
+                            
+                            Spacer()
+                            
+                            Text("\(whiteRosesQuantity)")
+                                .padding(.trailing, 4)
+                        }
+                    }
+                } header: {
+                    Text("Rosas")
                 }
             }
         }
